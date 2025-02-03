de585502a1e1a6364ee0fd66b8ccc9c9b85339d1
Hammoude Rawad <97172614+rawadh92@users.noreply.github.com>
2025-02-03 10:50:58 +0100

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,19 +1,1 @@
-from sqlalchemy.orm import Session
-from app import models, schemas
-from app.database import SessionLocal
 
-def get_houses():
-    session = SessionLocal()
-    try:
-        return session.query(models.House).all()
-    finally:
-        session.close()
-
-def create_house(house: schemas.HouseCreate):
-    session = SessionLocal()
-    db_house = models.House(**house.dict())
-    session.add(db_house)
-    session.commit()
-    session.refresh(db_house)
-    session.close()
-    return db_house
