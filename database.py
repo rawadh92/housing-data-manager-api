518697e7d99a4a2c288e2a85a384d5416d1dedd5
Hammoude Rawad <97172614+rawadh92@users.noreply.github.com>
2025-02-03 10:51:45 +0100

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-# Configuration de la base de données via les variables d'environnement
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/housing")
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
